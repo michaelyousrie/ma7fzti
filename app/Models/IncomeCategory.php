b7a7160b81ff255d1b5d4d3d23b1ecfcb7671501
Michael <maikdevelops@gmail.com>
2019-04-04 00:55:50 +0200

    Added models.

---
+++ app/Models/IncomeCategory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IncomeCategory extends Model
+{
+    protected $table = "income_categories";
+}
---
+++ app/Models/UserSubscription.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserSubscription extends Model
+{
+    protected $table = "user_subscriptions";
+}
