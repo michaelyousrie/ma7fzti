0b425d2a6faaca2152556440930c36736fb38ee4
Michael <maikdevelops@gmail.com>
2019-04-08 12:57:22 +0200

    update expense factory to create random floats.

--- database/factories/ExpenseFactory.php
+++ database/factories/ExpenseFactory.php
@@ -8,7 +8,7 @@ use App\Models\Expense;
 $factory->define(Expense::class, function (Faker $faker) {
     return [
         'user_id'       => factory( User::class )->create()->id,
-        "amount"        => random_int(10, 100),
+        "amount"        => round(random_int(10, 100)/random_int(1, 9), 2),
         "category_id"   => factory( ExpenseCategory::class )->create()->id,
         "description"   => $faker->sentence
     ];
