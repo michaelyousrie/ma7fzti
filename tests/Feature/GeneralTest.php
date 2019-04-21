6e67d04933714a3e5c5172f7c088ed44045718e2
Michael <maikdevelops@gmail.com>
2019-04-22 01:32:53 +0200

    test login/registration.

--- tests/Feature/GeneralTest.php
+++ tests/Feature/GeneralTest.php
@@ -3,8 +3,11 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Language;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+
 
 class GeneralTest extends TestCase
 {
@@ -16,5 +19,65 @@ class GeneralTest extends TestCase
         $resp = $this->get('/api/v1/languages');
 
         $this->assertEquals($resp->status(), 403);
+    }
+
+
+    public function testSendingRequestToLoginAndRegisterWithoutApiTokenWorks()
+    {
+        $resp = $this->post('/api/v1/login');
+
+        $this->assertTrue( $resp->status() != 403 );
+
+        $resp = $this->post('/api/v1/register');
+
+        $this->assertTrue( $resp->status() != 403 );
+    }
+
+
+    public function testRegistration()
+    {
+        $language = factory(Language::class)->create();
+
+        $data = [
+            'first_name'    => "Test",
+            "last_name"     => "Testt",
+            "email"         => "test@test.com",
+            "password"      => "password",
+            "currency"      => "EGP",
+            "language_code" => $language->code
+        ];
+
+        $resp = (array) json_decode( $this->post( "/api/v1/register", $data )->getContent() );
+
+        $this->assertArrayHasKey("data", $resp);
+
+        $id = $resp['data']->id;
+
+        $this->assertDatabaseHas('users', [
+            "first_name"        => "Test",
+            "last_name"         => "Testt",
+            "id"                => $id
+        ]);
+    }
+
+
+    public function testLogin()
+    {
+        $user = factory( User::class )->create();
+
+        $data = [
+            "email"     => $user->email,
+            "password"  => "password"
+        ];
+
+        $resp = (array) json_decode( $this->post( "/api/v1/login", $data )->getContent() );
+
+        $this->assertArrayHasKey("data", $resp);
+
+        $resp = $resp['data'];
+
+        $this->assertEquals( $user->id, $resp->id );
+        $this->assertEquals( $user->first_name, $resp->first_name );
+        $this->assertEquals( $user->last_name, $resp->last_name );
     }
 }
